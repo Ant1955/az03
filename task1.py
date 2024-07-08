7a93e1e3a2e99fdd303a147dfdc5bcf2787ccf3b
Ant1955 <abb1955@gmail.com>
2024-07-08 12:13:41 +0300

    task_1

--- task1.py
+++ task1.py
@@ -8,7 +8,6 @@
 # # Генерация случайных чисел, распределенных по нормальному распределению
 # data = np.random.normal(mean, std_dev, num_samples)
 #
-
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -17,7 +16,6 @@ std_dev = 1  # Стандартное отклонение
 num_samples = 1000  # Количество образцов
 # Генерация случайных чисел, распределенных по нормальному распределению
 data = np.random.normal(mean, std_dev, num_samples)
-
 
 # Построение гистограммы
 plt.hist(data, bins=20, edgecolor='black')
--- task2.py
+++ task2.py
@@ -1,15 +1,23 @@
 # 2. Построй диаграмму рассеяния для двух наборов случайных данных,
-# сгенерированных с помощью функции `numpy.random.rand`.​
+# сгенерированных с помощью функции `numpy.random.rand`.
 #
 # import numpy as np
 # random_array = np.random.rand(5) # массив из 5 случайных чисел
 # print(random_array)
 #
-import requests
-import csv
-from bs4 import BeautifulSoup
-import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
-from datetime import datetime
 
+random_array_x = np.random.rand(50) # массив x из 50 случайных чисел
+random_array_y = np.random.rand(50) # массив y из 50 случайных чисел
+
+# Строим диаграмму рассеяния:
+plt.scatter(random_array_x, random_array_y)
+
+# Кастомизируем график:
+plt.xlabel("ось Х")
+plt.ylabel("ось Y")
+plt.title("Тестовая диаграмма рассеяния")
+
+# Показываем график:
+plt.show()
